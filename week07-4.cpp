f2dc29979265e2c1f4c14d380cb7862722ababaf
linlinwy <114201442+linlinwy@users.noreply.github.com>
2022-10-21 11:10:33 +0800

    week07

---
+++ week07-4.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int n=123456789;
+    printf("计琌%d\n",n%10);
+    n = n/10;
+
+    printf("计琌%d\n",n%10);
+    n = n/10;
+
+    printf("计琌%d\n",n%10);
+    n = n/10;
+
+    printf("计琌%d\n",n%10);
+    n = n/10;
+
+    printf("计琌%d\n",n%10);
+    n = n/10;
+
+    printf("计琌%d\n",n%10);
+    n = n/10;
+
+    printf("计琌%d\n",n%10);
+    n = n/10;
+
+    printf("计琌%d\n",n%10);
+    n = n/10;
+
+    printf("计琌%d\n",n%10);
+    n = n/10;
+}
